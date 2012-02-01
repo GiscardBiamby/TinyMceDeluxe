3b4cb53013bd88c0f66d29e7355e84d602ede69c
Giscard <Giscard@Terrax>
2012-02-01 11:54:53 -0500

    - Deleted external-plugins-loader.js (moved the contents of that to Scripts/samples/orchard-tinymce.js. Makes it easier to use this module (you only have to override one file instead of two)

--- src/TinyMceDeluxe/ResourceManifest.cs
+++ src/TinyMceDeluxe/ResourceManifest.cs
@@ -4,9 +4,7 @@ namespace TinyMceDeluxe {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
-
             manifest.DefineScript("TinyMce").SetUrl("tiny_mce.js", "tiny_mce_src.js").SetVersion("3.4.7");
-            manifest.DefineScript("TinyMceDeluxePlugins").SetUrl("external-plugin-loader.js").SetDependencies("TinyMce");
         }
 
     }
