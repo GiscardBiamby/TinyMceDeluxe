87744713f5b2ecb36fced3022061b98b693e571f
Giscard <Giscard@Terrax>
2012-01-31 23:22:10 -0500

    Initial commit

---
+++ src/TinyMceDeluxe/ResourceManifest.cs
@@ -0,0 +1,13 @@
+﻿using Orchard.UI.Resources;
+
+namespace TinyMceDeluxe {
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            manifest.DefineScript("TinyMce").SetUrl("tiny_mce.js", "tiny_mce_src.js").SetVersion("3.4.7");
+            manifest.DefineScript("TinyMceDeluxePlugins").SetUrl("external-plugin-loader.js").SetDependencies("TinyMce");
+        }
+
+    }
+}
---
+++ src/TinyMceDeluxe/Scripts/orchard-tinymce.js
@@ -0,0 +1,19 @@
+﻿//tinyMCE.init({
+//    theme: "advanced",
+//    mode: "specific_textareas",
+//    editor_selector: "tinymce",
+//    plugins: "fullscreen,searchreplace,mediapicker,-table,-pagebreak,-template",
+//    theme_advanced_toolbar_location: "top",
+//    theme_advanced_toolbar_align: "left",
+//    theme_advanced_statusbar_location: "bottom",
+//    theme_advanced_resizing: "true",
+//    theme_advanced_buttons1: "search,replace,|,cut,copy,paste,|,undo,redo,|,link,unlink,charmap,emoticon,codeblock,|,bold,italic,|,numlist,bullist,formatselect,|,styleselect,|,code,fullscreen",
+//    theme_advanced_buttons2: "mediapicker,|,tablecontrols,|,hr,removeformat,visualaid,|,visualchars,template,blockquote,pagebreak",
+//    theme_advanced_buttons3: "",
+//    convert_urls: false,
+//    content_css: "/Themes/GETheme/Styles/ge-main.css",
+//    template_external_list_url : "/Modules/gradedge/Scripts/tinymce_template_list.js",
+//    valid_elements: "*[*]",
+//    // shouldn't be needed due to the valid_elements setting, but TinyMCE would strip script.src without it.
+//    extended_valid_elements: "script[type|defer|src|language]"
+//});
