2b729c5f3bb65bfa109e460dcca56ceb2a3d9fe4
Giscard <Giscard@Terrax>
2012-02-06 10:34:34 -0500

    v 0.1.4 Minor edits to sample orchard-tinymce.js

--- src/TinyMceDeluxe/Scripts/samples/orchard-tinymce.js
+++ src/TinyMceDeluxe/Scripts/samples/orchard-tinymce.js
@@ -1,42 +1,44 @@
 ﻿////
-//// Copy this file to your module's Scripts folder, and declare it in your ResourceManifest.cs. This will override the copy in Orchard's default TinyMce module. 
+////
+//// Copy this file to your module's Scripts folder, and declare it in your ResourceManifest.cs. This will override the copy in Orchard's default TinyMce module.
 
 ////
+////
 //// Un-comment-out the tinymce.PluginManager.load commands for the plugins you want to use: 
-//tinymce.PluginManager.load('advhr', '/Modules/TinyMceDeluxe/Scripts/plugins/advhr/editor_plugin.js');
-//tinymce.PluginManager.load('advimage', '/Modules/TinyMceDeluxe/Scripts/plugins/advimage/editor_plugin.js');
-//tinymce.PluginManager.load('advlink', '/Modules/TinyMceDeluxe/Scripts/plugins/advlink/editor_plugin.js');
-//tinymce.PluginManager.load('advlist', '/Modules/TinyMceDeluxe/Scripts/plugins/advlist/editor_plugin.js');
-//tinymce.PluginManager.load('autolink', '/Modules/TinyMceDeluxe/Scripts/plugins/autolink/editor_plugin.js');
-//tinymce.PluginManager.load('autosave', '/Modules/TinyMceDeluxe/Scripts/plugins/autosave/editor_plugin.js');
-//tinymce.PluginManager.load('bbcode', '/Modules/TinyMceDeluxe/Scripts/plugins/bbcode/editor_plugin.js');
-//tinymce.PluginManager.load('contextmenu', '/Modules/TinyMceDeluxe/Scripts/plugins/contextmenu/editor_plugin.js');
-//tinymce.PluginManager.load('directionality', '/Modules/TinyMceDeluxe/Scripts/plugins/directionality/editor_plugin.js');
-//tinymce.PluginManager.load('emotions', '/Modules/TinyMceDeluxe/Scripts/plugins/emotions/editor_plugin.js');
-//tinymce.PluginManager.load('example', '/Modules/TinyMceDeluxe/Scripts/plugins/example/editor_plugin.js');
-//tinymce.PluginManager.load('fullpage', '/Modules/TinyMceDeluxe/Scripts/plugins/fullpage/editor_plugin.js');
-//tinymce.PluginManager.load('iespell', '/Modules/TinyMceDeluxe/Scripts/plugins/iespell/editor_plugin.js');
-//tinymce.PluginManager.load('inlinepopups', '/Modules/TinyMceDeluxe/Scripts/plugins/inlinepopups/editor_plugin.js');
-//tinymce.PluginManager.load('insertdatetime', '/Modules/TinyMceDeluxe/Scripts/plugins/insertdatetime/editor_plugin.js');
-//tinymce.PluginManager.load('layer', '/Modules/TinyMceDeluxe/Scripts/plugins/layer/editor_plugin.js');
-//tinymce.PluginManager.load('legacyoutput', '/Modules/TinyMceDeluxe/Scripts/plugins/legacyoutput/editor_plugin.js');
-//tinymce.PluginManager.load('lists', '/Modules/TinyMceDeluxe/Scripts/plugins/lists/editor_plugin.js');
-//tinymce.PluginManager.load('media', '/Modules/TinyMceDeluxe/Scripts/plugins/media/editor_plugin.js');
-//tinymce.PluginManager.load('nonbreaking', '/Modules/TinyMceDeluxe/Scripts/plugins/nonbreaking/editor_plugin.js');
-//tinymce.PluginManager.load('noneditable', '/Modules/TinyMceDeluxe/Scripts/plugins/noneditable/editor_plugin.js');
-//tinymce.PluginManager.load('pagebreak', '/Modules/TinyMceDeluxe/Scripts/plugins/pagebreak/editor_plugin.js');
-//tinymce.PluginManager.load('paste', '/Modules/TinyMceDeluxe/Scripts/plugins/paste/editor_plugin.js');
-//tinymce.PluginManager.load('preview', '/Modules/TinyMceDeluxe/Scripts/plugins/preview/editor_plugin.js');
-//tinymce.PluginManager.load('print', '/Modules/TinyMceDeluxe/Scripts/plugins/print/editor_plugin.js');
-//tinymce.PluginManager.load('save', '/Modules/TinyMceDeluxe/Scripts/plugins/save/editor_plugin.js');
-//tinymce.PluginManager.load('spellchecker', '/Modules/TinyMceDeluxe/Scripts/plugins/spellchecker/editor_plugin.js');
-//tinymce.PluginManager.load('style', '/Modules/TinyMceDeluxe/Scripts/plugins/style/editor_plugin.js');
-//tinymce.PluginManager.load('tabfocus', '/Modules/TinyMceDeluxe/Scripts/plugins/tabfocus/editor_plugin.js');
-//tinymce.PluginManager.load('table', '/Modules/TinyMceDeluxe/Scripts/plugins/table/editor_plugin.js');
-//tinymce.PluginManager.load('template', '/Modules/TinyMceDeluxe/Scripts/plugins/template/editor_plugin.js');
-//tinymce.PluginManager.load('visualchars', '/Modules/TinyMceDeluxe/Scripts/plugins/visualchars/editor_plugin.js');
-//tinymce.PluginManager.load('wordcount', '/Modules/TinyMceDeluxe/Scripts/plugins/wordcount/editor_plugin.js');
-//tinymce.PluginManager.load('xhtmlxtras', '/Modules/TinyMceDeluxe/Scripts/plugins/xhtmlxtras/editor_plugin.js');
+////tinymce.PluginManager.load('advhr', '/Modules/TinyMceDeluxe/Scripts/plugins/advhr/editor_plugin.js');
+////tinymce.PluginManager.load('advimage', '/Modules/TinyMceDeluxe/Scripts/plugins/advimage/editor_plugin.js');
+////tinymce.PluginManager.load('advlink', '/Modules/TinyMceDeluxe/Scripts/plugins/advlink/editor_plugin.js');
+////tinymce.PluginManager.load('advlist', '/Modules/TinyMceDeluxe/Scripts/plugins/advlist/editor_plugin.js');
+////tinymce.PluginManager.load('autolink', '/Modules/TinyMceDeluxe/Scripts/plugins/autolink/editor_plugin.js');
+////tinymce.PluginManager.load('autosave', '/Modules/TinyMceDeluxe/Scripts/plugins/autosave/editor_plugin.js');
+////tinymce.PluginManager.load('bbcode', '/Modules/TinyMceDeluxe/Scripts/plugins/bbcode/editor_plugin.js');
+////tinymce.PluginManager.load('contextmenu', '/Modules/TinyMceDeluxe/Scripts/plugins/contextmenu/editor_plugin.js');
+////tinymce.PluginManager.load('directionality', '/Modules/TinyMceDeluxe/Scripts/plugins/directionality/editor_plugin.js');
+////tinymce.PluginManager.load('emotions', '/Modules/TinyMceDeluxe/Scripts/plugins/emotions/editor_plugin.js');
+////tinymce.PluginManager.load('example', '/Modules/TinyMceDeluxe/Scripts/plugins/example/editor_plugin.js');
+////tinymce.PluginManager.load('fullpage', '/Modules/TinyMceDeluxe/Scripts/plugins/fullpage/editor_plugin.js');
+////tinymce.PluginManager.load('iespell', '/Modules/TinyMceDeluxe/Scripts/plugins/iespell/editor_plugin.js');
+////tinymce.PluginManager.load('inlinepopups', '/Modules/TinyMceDeluxe/Scripts/plugins/inlinepopups/editor_plugin.js');
+////tinymce.PluginManager.load('insertdatetime', '/Modules/TinyMceDeluxe/Scripts/plugins/insertdatetime/editor_plugin.js');
+////tinymce.PluginManager.load('layer', '/Modules/TinyMceDeluxe/Scripts/plugins/layer/editor_plugin.js');
+////tinymce.PluginManager.load('legacyoutput', '/Modules/TinyMceDeluxe/Scripts/plugins/legacyoutput/editor_plugin.js');
+////tinymce.PluginManager.load('lists', '/Modules/TinyMceDeluxe/Scripts/plugins/lists/editor_plugin.js');
+////tinymce.PluginManager.load('media', '/Modules/TinyMceDeluxe/Scripts/plugins/media/editor_plugin.js');
+////tinymce.PluginManager.load('nonbreaking', '/Modules/TinyMceDeluxe/Scripts/plugins/nonbreaking/editor_plugin.js');
+////tinymce.PluginManager.load('noneditable', '/Modules/TinyMceDeluxe/Scripts/plugins/noneditable/editor_plugin.js');
+////tinymce.PluginManager.load('pagebreak', '/Modules/TinyMceDeluxe/Scripts/plugins/pagebreak/editor_plugin.js');
+////tinymce.PluginManager.load('paste', '/Modules/TinyMceDeluxe/Scripts/plugins/paste/editor_plugin.js');
+////tinymce.PluginManager.load('preview', '/Modules/TinyMceDeluxe/Scripts/plugins/preview/editor_plugin.js');
+////tinymce.PluginManager.load('print', '/Modules/TinyMceDeluxe/Scripts/plugins/print/editor_plugin.js');
+////tinymce.PluginManager.load('save', '/Modules/TinyMceDeluxe/Scripts/plugins/save/editor_plugin.js');
+////tinymce.PluginManager.load('spellchecker', '/Modules/TinyMceDeluxe/Scripts/plugins/spellchecker/editor_plugin.js');
+////tinymce.PluginManager.load('style', '/Modules/TinyMceDeluxe/Scripts/plugins/style/editor_plugin.js');
+////tinymce.PluginManager.load('tabfocus', '/Modules/TinyMceDeluxe/Scripts/plugins/tabfocus/editor_plugin.js');
+////tinymce.PluginManager.load('table', '/Modules/TinyMceDeluxe/Scripts/plugins/table/editor_plugin.js');
+////tinymce.PluginManager.load('template', '/Modules/TinyMceDeluxe/Scripts/plugins/template/editor_plugin.js');
+////tinymce.PluginManager.load('visualchars', '/Modules/TinyMceDeluxe/Scripts/plugins/visualchars/editor_plugin.js');
+////tinymce.PluginManager.load('wordcount', '/Modules/TinyMceDeluxe/Scripts/plugins/wordcount/editor_plugin.js');
+////tinymce.PluginManager.load('xhtmlxtras', '/Modules/TinyMceDeluxe/Scripts/plugins/xhtmlxtras/editor_plugin.js');
 
 ////
 ////
@@ -49,7 +51,7 @@
 //    theme: "advanced",
 //    mode: "specific_textareas",
 //    editor_selector: "tinymce",
-//    plugins: "fullscreen,autoresize,searchreplace,mediapicker,-table,-pagebreak,-template",
+//    plugins: "fullscreen,autoresize,searchreplace,mediapicker,-table,-pagebreak,-template,-paste",
 //    theme_advanced_toolbar_location: "top",
 //    theme_advanced_toolbar_align: "left",
 //    theme_advanced_statusbar_location: "bottom",
